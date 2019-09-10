f889a9c918d99a06d3207133ea7c61be5c592602
Connor Meth <connormeth27@gmail.com>
2019-09-10 19:50:59 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  Array.new(array.size) { |i| array[i] ** 2}
 end
+
+numbers = [1, 2, 3]
+square_array(numbers)
+new_numbers = [9, 10, 16, 25]
+square_array(new_numbers)
